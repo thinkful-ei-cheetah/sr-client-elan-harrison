e9a6e82b29b386d604e653435ce542047eff4836
elangreen <green.elan5@gmail.com>
2019-06-24 08:44:59 -0700

    added dashboard component blank

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Dashboard extends Component {
+  render() {
+    return (
+      <section>
+        
+      </section>
+    )
+  }
+}
+
+export default Dashboard
