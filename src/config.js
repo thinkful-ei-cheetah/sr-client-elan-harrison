fee3a942b3bb19fb9550328181bb715eca61f557
elangreen <green.elan5@gmail.com>
2019-06-28 11:59:31 -0700

    api domain changed

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://murmuring-temple-44523.herokuapp.com/api',
+  API_ENDPOINT: 'https://space-repetition-api.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
-// https://murmuring-temple-44523.herokuapp.com/api
